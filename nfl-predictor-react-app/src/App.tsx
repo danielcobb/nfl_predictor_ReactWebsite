73259d7b96def05e4d9914d01472d801d5ac2d39
danielcobb <danielcobb20@gmail.com>
2025-12-13 15:41:14 -0500

    edited website title and added list layout for games

--- nfl-predictor-react-app/src/App.tsx
+++ nfl-predictor-react-app/src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
-import SelectMenu from "./components/selectMenu";
+import SelectMenu from "./components/SelectMenu";
+import GameList from "./components/GameList";
 
 function App() {
   return (
     <div>
       <Header>NFL Game Predictor</Header>
       <SelectMenu />
+      <GameList />
     </div>
   );
 }
---
+++ nfl-predictor-react-app/src/components/GameList.tsx
@@ -0,0 +1,26 @@
+const GameList = () => {
+  return (
+    <>
+      <ul className="list-group">
+        <li className="list-group-item">Game 1</li>
+        <li className="list-group-item">Game 2</li>
+        <li className="list-group-item">Game 3</li>
+        <li className="list-group-item">Game 4</li>
+        <li className="list-group-item">Game 5</li>
+        <li className="list-group-item">Game 6</li>
+        <li className="list-group-item">Game 7</li>
+        <li className="list-group-item">Game 8</li>
+        <li className="list-group-item">Game 9</li>
+        <li className="list-group-item">Game 10</li>
+        <li className="list-group-item">Game 11</li>
+        <li className="list-group-item">Game 12</li>
+        <li className="list-group-item">Game 13</li>
+        <li className="list-group-item">Game 14</li>
+        <li className="list-group-item">Game 15</li>
+        <li className="list-group-item">Game 16</li>
+      </ul>
+    </>
+  );
+};
+
+export default GameList;
