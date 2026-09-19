8f4ca6942f67751238628408df8cc471bf5fec90
danielcobb <danielcobb20@gmail.com>
2026-09-18 23:50:00 -0400

    added favicon

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",  # Vite default
         "http://127.0.0.1:5173",
-        "https://danielcobb.github.io"
+        "https://danielcobb.github.io",
+        "https://nfl-predictor-react-website-htolen3zt-danielcobbs-projects.vercel.app/"
     ],
     allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
